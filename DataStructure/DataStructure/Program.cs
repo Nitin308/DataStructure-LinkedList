f1fb27706fcc9eb6ff93709aa104f857546f4eae
Nitin308 <nitin3081998@gmail.com>
2022-12-06 19:46:39 +0530

    Search Linked List to find Node position

---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList");
+
+            LinkedList objLinkedList = new LinkedList();
+            objLinkedList.Add(56);
+            objLinkedList.Add(36);
+            objLinkedList.Add(70);
+            objLinkedList.Search(36);
+            objLinkedList.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
