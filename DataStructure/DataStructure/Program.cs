796f4a06120b17df7349356e6e346de3f52ddfef
Nitin308 <nitin3081998@gmail.com>
2022-12-06 20:22:46 +0530

    Remove element at a given position

---
+++ DataStructure/DataStructure/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList");
+
+            LinkedList objLinkedList = new LinkedList();
+            objLinkedList.Add(56);
+            objLinkedList.Add(36);
+            objLinkedList.Add(40);
+            objLinkedList.Add(70);
+            objLinkedList.RemoveAtPosition(3);
+            objLinkedList.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
