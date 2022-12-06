65bbedd1086dc5692714aec02e479c5b093bf90d
Nitin308 <nitin3081998@gmail.com>
2022-12-06 17:01:29 +0530

    Create Linked List by adding the numbers to it

---
+++ DataStructure/LinkedList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        public void Add(int data)
+        {
+            Node objNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = objNode;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = objNode;
+            }
+            Console.WriteLine($"\n{objNode.data} inserted into Linked List");
+        }
+
+        public void InsertFront(int new_data)
+        {
+
+            Node objnew_node = new Node(new_data);
+            objnew_node.next = this.head;
+            this.head = objnew_node;
+            Console.WriteLine($"\n{objnew_node.data} is added at front");
+        }
+
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("\nLinkedList is empty");
+                return;
+            }
+            Console.Write("\nLinked List => ");
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ DataStructure/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to LinkedList");
+
+            LinkedList objLinkedList = new LinkedList();
+            objLinkedList.Add(70);
+            objLinkedList.InsertFront(30);
+            objLinkedList.InsertFront(56);
+            objLinkedList.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
